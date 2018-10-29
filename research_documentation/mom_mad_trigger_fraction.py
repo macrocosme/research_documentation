6720f5f154a56b0859914fb6f7dcb0188b96e896
D Vohl <vohl@astron.nl>
2018-10-29 11:48:22 +0100

    Fix figure

--- research_documentation/mom_mad_trigger_fraction.py
+++ research_documentation/mom_mad_trigger_fraction.py
@@ -1,6 +1,8 @@
 #!/bin/python3
 import numpy as np
 
+from tqdm import tqdm as progressbar
+
 from matplotlib import pyplot as plt
 from matplotlib import rc
 
@@ -63,29 +65,40 @@ def draw(n_samples=2500):
 def snr_median_mad(n_trials=1000, downsampling_levels=250, n_samples=2500):
     SNRs = np.zeros([downsampling_levels, n_trials])
 
-    for trial in range(n_trials):
+    print ("Computing SNR [median and mad] - %s trials" % n_trials)
+    for trial in progressbar(range(n_trials), ascii=True):
         distribution = draw(n_samples)
         for level in range(downsampling_levels):
             distribution = downsample(distribution, level)
             med = np.median(distribution)
-            SNRs[level, trial] = (np.max(distribution) - med) / (compute_mad(distribution, med) * 1.48)
+            mad = compute_mad(distribution, med)
+            if mad != 0:
+                SNRs[level, trial] = (np.max(distribution) - med) / (mad * 1.48)
+            else:
+                SNRs[level, trial] = 0
     return SNRs
 
 
 def snr_mom_mad(n_trials=1000, downsampling_levels=250, n_samples=2500):
     SNRs = np.zeros([downsampling_levels, n_trials])
 
-    for trial in range(n_trials):
+    print("Computing SNR [mom and mad] - %s trials" % n_trials)
+    for trial in progressbar(range(n_trials), ascii=True):
         distribution = draw(n_samples)
         for level in range(downsampling_levels):
             distribution = downsample(distribution, level)
             mom = compute_mom(distribution)
-            SNRs[level, trial] = (np.max(distribution) - mom) / (compute_mad(distribution, mom) * 1.48)
+            mad = compute_mad(distribution, mom)
 
+            if mad != 0:
+                SNRs[level, trial] = (np.max(distribution) - mom) / (mad * 1.48)
+            else:
+                SNRs[level, trial] = 0
     return SNRs
 
 
-def plot_trigger_ratio(SNRs, threshold=10, figure_name="trigger_ratio_median"):
+def plot_trigger_ratio_atomic(SNRs, threshold=10, n_trials=1000, n_samples=2500, mode='median',
+                              figure_name="trigger_ratio"):
     fig, ax = plt.subplots()
 
     for level in range(SNRs.shape[0]):
@@ -101,10 +114,65 @@ def plot_trigger_ratio(SNRs, threshold=10, figure_name="trigger_ratio_median"):
         ax.set_xlabel("N")
         ax.set_ylabel("Trigger fraction (SNR>" + str(threshold) + ")")
 
-        plt.savefig(figure_name + "_threshold_" + str(threshold) + ".pdf")
+    filename = "%s_%s_threshold_%s.pdf" % (figure_name, mode, threshold)
+    print("Saving file %s" % filename)
+    plt.tight_layout()
+    plt.savefig(filename)
 
 
-def plot_max_snr_per_downsampling_level(SNRs, figure_name="max_snr_per_downsampling_median"):
+def plot_trigger_ratio(SNRs_median, SNRs_mom, threshold=10, n_trials=1000, n_samples=2500, figure_name="trigger_ratio"):
+    fig, ax = plt.subplots()
+
+    labels = ['median+mad', 'mom+mad']
+    colors = ['black', 'blue']
+    marker = ['+', 'o']
+
+    i = 0
+    for SNRs in [SNRs_median, SNRs_mom]:
+        first = True
+        for level in range(SNRs.shape[0]):
+            if level != 0:
+                if first:
+                    ax.scatter(n_samples / level,
+                               len(SNRs[level][np.where(SNRs[level] > threshold)]) / n_trials,
+                               marker=marker[i],
+                               color=colors[i],
+                               label=labels[i],
+                               )
+                else:
+                    ax.scatter(n_samples / level,
+                               len(SNRs[level][np.where(SNRs[level] > threshold)]) / n_trials,
+                               marker=marker[i],
+                               color=colors[i],
+                               )
+            else:
+                if first:
+                    ax.scatter(n_samples,
+                               len(SNRs[level][np.where(SNRs[level] > threshold)]) / n_trials,
+                               marker=marker[i],
+                               color=colors[i],
+                               label=labels[i],
+                               )
+                else:
+                    ax.scatter(n_samples,
+                               len(SNRs[level][np.where(SNRs[level] > threshold)]) / n_trials,
+                               marker=marker[i],
+                               color=colors[i],
+                               )
+            ax.set_xlabel("N")
+            ax.set_ylabel("Trigger fraction (SNR>" + str(threshold) + ")")
+            first = False
+        i += 1
+
+    ax.legend()
+    filename = "%s_threshold_%s.pdf" % (figure_name, threshold)
+    print("Saving file %s" % filename)
+    plt.tight_layout()
+    plt.savefig(filename)
+
+
+def plot_max_snr_per_downsampling_level_atomic(SNRs, n_samples=2500, mode='median',
+                                               figure_name="max_snr_per_downsampling"):
     fig, ax = plt.subplots()
 
     for level in range(SNRs.shape[0]):
@@ -120,9 +188,64 @@ def plot_max_snr_per_downsampling_level(SNRs, figure_name="max_snr_per_downsampl
         ax.set_xlabel("N")
         ax.set_ylabel("Max(SNR)")
 
-    plt.savefig(figure_name + ".pdf")
+    filename = "%s_%s.pdf" % (figure_name, mode)
+    print("Saving file %s" % filename)
+    plt.tight_layout()
+    plt.savefig(filename)
 
-if __name__ == "__main__":
+
+def plot_max_snr_per_downsampling_level(SNRs_median, SNRs_mom, n_samples=2500, figure_name="max_snr_per_downsampling"):
+    fig, ax = plt.subplots()
+
+    labels = ['median+mad', 'mom+mad']
+    colors = ['black', 'blue']
+    marker = ['+', 'o']
+
+    i = 0
+    for SNRs in [SNRs_median, SNRs_mom]:
+        first = True
+        for level in range(SNRs.shape[0]):
+            if level != 0:
+                if first:
+                    ax.scatter(n_samples / level,
+                               np.max(SNRs[level]),
+                               marker=marker[i],
+                               color=colors[i],
+                               label=labels[i],
+                               )
+                else:
+                    ax.scatter(n_samples / level,
+                               np.max(SNRs[level]),
+                               marker=marker[i],
+                               color=colors[i],
+                               )
+            else:
+                if first:
+                    ax.scatter(n_samples,
+                               np.max(SNRs[level]),
+                               marker=marker[i],
+                               color=colors[i],
+                               label=labels[i],
+                               )
+                else:
+                    ax.scatter(n_samples,
+                               np.max(SNRs[level]),
+                               marker=marker[i],
+                               color=colors[i],
+                               )
+            first = False
+            ax.set_xlabel("N")
+            ax.set_ylabel("Max(SNR)")
+        i += 1
+
+    ax.legend()
+    filename = "%s.pdf" % (figure_name)
+    print("Saving file %s" % filename)
+    plt.tight_layout()
+    plt.savefig(filename)
+
+
+def main():
     n_trials = 1000
     downsampling_levels = 250
     n_samples = 2500
@@ -130,9 +253,9 @@ if __name__ == "__main__":
     SNR_median = snr_median_mad(n_trials, downsampling_levels, n_samples)
     SNR_mom = snr_mom_mad(n_trials, downsampling_levels, n_samples)
 
-    plot_trigger_ratio(SNR_median, 6)
-    plot_trigger_ratio(SNR_mom, 6, figure_name="trigger_ratio_median")
+    plot_trigger_ratio(SNR_median, SNR_mom, 6)
+    plot_max_snr_per_downsampling_level(SNR_median, SNR_mom)
 
-    plot_max_snr_per_downsampling_level(SNR_median)
-    plot_max_snr_per_downsampling_level(SNR_mom, figure_name="max_snr_per_downsampling_mom")
+if __name__ == "__main__":
+    main()
 

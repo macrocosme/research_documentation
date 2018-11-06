614a35132768106fe41415a15f604cb6dc720bb6
Dany Vohl <vohl@arts005.apertif>
2018-11-06 14:33:52 +0000

    Runnable code

--- research_documentation/mad_stdev_filterbank.py
+++ research_documentation/mad_stdev_filterbank.py
@@ -1,10 +1,8 @@
 import numpy as np
-from matplotlib.pyplot import plot, legend, ylabel, xlabel, semilogx
-from arts_analysis import reader
+import argparse
 
 def read_fil_data(fn):
     x = reader.read_fil_data(fn, start=0, stop=250000)[0]
-    print(x.data.shape)
     return x
 
 def compute_mad(array, med):
@@ -14,7 +12,7 @@ def compute_mad(array, med):
 def compute_mom(L):
     if len(L) < 10:
         L.sort()
-        return L[int(len(L) / 2)]
+        return np.median(L)
     S = []
     l_index = 0
     for l_index in range(0, len(L) - 1, 5):
@@ -38,62 +36,87 @@ def compute_mom(L):
         return L2
 
 def main():
-    # THE MAIN EVENT IS HAPPENING HERE.
-    x = read_fil_data('/data2/output/20181020_2018-10-20-09:59:59.FRB171004_filterbank/CB17.fil')
-    k = len(x)
+    from arts_analysis import reader
+    import copy
 
+    # THE MAIN EVENT IS HAPPENING HERE.
     N = np.array([1, 5, 10, 50, 100, 250, 500, 1000, 2500])
+    data = reader.read_fil_data('/data2/output/20181020_2018-10-20-09:59:59.FRB171004_filterbank/CB17.fil', start=0, stop=250000)[0]
 
     S1, S2, S3, S4 = [],[],[],[]
 
-    # median + stdev
+    SS1, SS2, SS3, SS4 = {},{},{},{}
+
+    # Initialise dicts
+    for ss in [SS1, SS2, SS3, SS4]:
+        for nn in N:
+            ss[nn] = -999999
+
+    for ii in range(1,1001, 10):
+        print (ii)
+        xx = copy.deepcopy(data)
+        xx.dedisperse(ii)
+        xx = np.mean(xx.data, axis=0)
+
+        for nn in N:
+            print ("\t%d" % nn)
+
+            k = len(xx)
+            x = xx[:k//nn*nn].reshape(-1, nn).mean(-1)
+
+            print ("\t\t...Step 1")
+            snr = (x.max() - np.median(x))/(np.std(x))
+            if snr > SS1[nn]:
+                SS1[nn] = snr
+
+            print ("\t\t...Step 2")
+            med = compute_mom(x)
+            snr = (x.max() - med)/(1.48*compute_mad(x, med))
+            if snr > SS2[nn]:
+                SS2[nn] = snr
+
+            print ("\t\t...Step 3")
+            med = np.median(x)
+            snr = (x.max() - med)/(1.48*compute_mad(x, med))
+            if snr > SS3[nn]:
+                SS3[nn] = snr
+
+            print ("\t\t...Step 4")
+            snr = (x.max() - compute_mom(x))/(np.std(x))
+            if snr > SS4[nn]:
+                SS4[nn] = snr
+
+            print ("")
+
+    print ("printing order of append")
     for nn in N:
-        d = []
-        x = x[:k//nn*nn].reshape(-1, nn).mean(-1)
-        snr = (x.max() - np.median(x))/(np.std(x))
-        d.append(snr)
-        d = np.array(d)
-        S1.append(d.max())
-        print(len(x), nn)
+        print (nn)
+        S1.append(SS1[nn])
+        S2.append(SS2[nn])
+        S3.append(SS3[nn])
+        S4.append(SS4[nn])
 
-    # MoM + 1.48*MAD with MoM (amber)
-    for nn in N:
-        d = []
-        x = x[:k//nn*nn].reshape(-1, nn).mean(-1)
-        med = compute_mom(x)
-        snr = (x.max() - med)/(1.48*compute_mad(x, med))
-        d.append(snr)
-        d = np.array(d)
-        S2.append(d.max())
+    return S1, S2, S3, S4, N
 
-    # median + 1.48*MAD
-    for nn in N:
-        d = []
-        x = x[:k//nn*nn].reshape(-1, nn).mean(-1)
-        med = np.median(x)
-        snr = (x.max() - med)/(1.48*compute_mad(x, med))
-        d.append(snr)
-        d = np.array(d)
-        S3.append(d.max())
+def make_plot(S1, S2, S3, S4, N, fn="filterbank.png"):
+    from matplotlib import pyplot as plt
+    plt.plot(25000./N, S1)
+    plt.plot(25000./N, S2)
+    plt.plot(25000./N, S3)
+    plt.plot(25000./N, S4)
 
-    # MoM + stdev
-    for nn in N:
-        d = []
-        x = x[:k//nn*nn].reshape(-1, nn).mean(-1)
-        snr = (x.max() - compute_mom(x))/(np.std(x))
-        d.append(snr)
-        d = np.array(d)
-        S4.append(d.max())
+    plt.legend(['median, stdev','MoM, MAD','median, MAD', 'MoM, stdev'], fontsize=20)
+    plt.ylabel('S/N Max (100 DMs)', fontsize=20)
+    plt.xlabel('N', fontsize=20)
 
+    # plt.yscale = 'log'
+    # plt.xscale = 'linear'
+    plt.semilogx()
 
-    plot(25000./N, S1)
-    plot(25000./N, S2)
-    plot(25000./N, S3)
-    plot(25000./N, S4)
+    plt.tight_layout()
+    plt.show()
+    plt.savefig(fn)
 
-    legend(['median, stdev','MoM, MAD','median, MAD', 'MoM, stdev'], fontsize=20)
-
-    ylabel('S/N Max samples', fontsize=20)
-    xlabel('N', fontsize=20)
-
-    semilogx()
+if __name__ == "__main__":
+    S1, S2, S3, S4, N = main()
+    make_plot(S1, S2, S3, S4, N)

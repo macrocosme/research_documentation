9378f274fe72d1f2e3f81aa2d124c047bde84a3e
D Vohl <vohl@astron.nl>
2018-11-07 11:09:14 +0000

    Add argparse

--- research_documentation/mad_stdev_filterbank.py
+++ research_documentation/mad_stdev_filterbank.py
@@ -1,6 +1,9 @@
 import numpy as np
 import argparse
 
+import cPickle as pickle
+import argparse
+
 def read_fil_data(fn):
     x = reader.read_fil_data(fn, start=0, stop=250000)[0]
     return x
@@ -35,11 +38,10 @@ def compute_mom(L):
     else:
         return L2
 
-def main():
+def main(**kwargs):
     from arts_analysis import reader
     import copy
 
-    # THE MAIN EVENT IS HAPPENING HERE.
     N = np.array([1, 5, 10, 50, 100, 250, 500, 1000, 2500])
     data = reader.read_fil_data('/data2/output/20181020_2018-10-20-09:59:59.FRB171004_filterbank/CB17.fil', start=0, stop=250000)[0]
 
@@ -52,7 +54,7 @@ def main():
         for nn in N:
             ss[nn] = -999999
 
-    for ii in range(1,1001, 10):
+    for ii in range(kwargs['N_DM']):
         print (ii)
         xx = copy.deepcopy(data)
         xx.dedisperse(ii)
@@ -98,7 +100,7 @@ def main():
 
     return S1, S2, S3, S4, N
 
-def make_plot(S1, S2, S3, S4, N, fn="filterbank.png"):
+def make_plot(S1, S2, S3, S4, N, **kwargs):
     from matplotlib import pyplot as plt
     plt.plot(25000./N, S1)
     plt.plot(25000./N, S2)
@@ -106,17 +108,48 @@ def make_plot(S1, S2, S3, S4, N, fn="filterbank.png"):
     plt.plot(25000./N, S4)
 
     plt.legend(['median, stdev','MoM, MAD','median, MAD', 'MoM, stdev'], fontsize=20)
-    plt.ylabel('S/N Max (100 DMs)', fontsize=20)
+    plt.ylabel('S/N Max (1000 DMs)', fontsize=15)
     plt.xlabel('N', fontsize=20)
 
-    # plt.yscale = 'log'
-    # plt.xscale = 'linear'
-    plt.semilogx()
+    plt.xscale = kwargs['xscale']
+    plt.yscale = kwargs['yscale']
 
     plt.tight_layout()
     plt.show()
-    plt.savefig(fn)
+    plt.savefig(kwargs['figure_filename'])
+
+def check_defaults(**kwargs):
+    if kwargs['xscale'] == None:
+        kwargs['xscale'] = 'log'
+
+    if kwargs['yscale'] == None:
+        kwargs['yscale'] = 'linear'
+
+    if kwargs['N_DM'] == None:
+        kwargs['N_DM'] = 1000
+
+    if kwargs['figure_filename'] == None:
+        kwargs['figure_filename'] = 'filterbank_%d.png' % kwargs['N_DM']
+
+    return kwargs
+
 
 if __name__ == "__main__":
-    S1, S2, S3, S4, N = main()
-    make_plot(S1, S2, S3, S4, N)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pickle_filename', help="Pickle filename")
+    parser.add_argument('--figure_filename', help="Figure filename", type=str)
+    parser.add_argument('--N_DM', help="Number of DM trials", type=int)
+    parser.add_argument('--xscale', help="Scaling for the x axis", type=str)
+    parser.add_argument('--yscale', help="Scaling for the y axis", type=str)
+    args = parser.parse_args()
+
+    kwargs = check_defaults(**vars(args))
+
+    S1, S2, S3, S4, N = main(**kwargs)
+
+    if args.pickle_filename:
+        with open(args.pickle_filename, 'wb') as f:
+            pickle.dump([S1, S2, S3, S4, N], f)
+
+    make_plot(S1, S2, S3, S4, N, **kwargs)

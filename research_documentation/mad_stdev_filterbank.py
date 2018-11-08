8ac6e6f7790213d57035cacfe4bdbdbcfc5ff1cf
D Vohl <vohl@astron.nl>
2018-11-08 11:20:39 +0000

    Add time chunks option

--- research_documentation/mad_stdev_filterbank.py
+++ research_documentation/mad_stdev_filterbank.py
@@ -2,10 +2,6 @@ import numpy as np
 import argparse
 import cPickle as pickle
 
-def read_fil_data(fn):
-    x = reader.read_fil_data(fn, start=0, stop=250000)[0]
-    return x
-
 def compute_mad(array, med):
     series_mad = np.abs(array - med)
     return np.median(series_mad)
@@ -41,8 +37,13 @@ def main(**kwargs):
     import copy
 
     N = np.array([1, 5, 10, 50, 100, 250, 500, 1000, 2500])
-    if not kwargs['gaussian_noise']:
-        data = reader.read_fil_data('/data2/output/20181020_2018-10-20-09:59:59.FRB171004_filterbank/CB17.fil', start=0, stop=250000)[0]
+    if kwargs['input_type'] == 'filterbank':
+        data = reader.read_fil_data(kwargs['filterbank_filename'], start=0, stop=250000)[0]
+    if kwargs['input_type'] == 'time_chunks':
+        chunk_size = 25000
+        index_start = 0
+
+        #chunk_size
 
     S1, S2, S3, S4 = [],[],[],[]
 
@@ -53,15 +54,21 @@ def main(**kwargs):
         for nn in N:
             ss[nn] = -999999
 
-    for ii in range(kwargs['N_DM']):
+    for ii in range(kwargs['n_samples']):
         print (ii)
 
-        if not kwargs['gaussian_noise']:
+        print kwargs['input_type']
+
+        if kwargs['input_type'] == 'filterbank':
             xx = copy.deepcopy(data)
             xx.dedisperse(ii)
             xx = np.mean(xx.data, axis=0)
-        else:
+        elif kwargs['input_type'] == 'gaussian_noise':
             xx = np.random.normal(0, 1, 25000)
+        elif kwargs['input_type'] == 'time_chunks':
+            xx = reader.read_fil_data(kwargs['filterbank_filename'], start=index_start, stop=index_start+kwargs['chunk_size'])[0]
+            xx = np.mean(xx.data, axis=0)
+            index_start += kwargs['chunk_size']
 
         for nn in N:
             print ("\t%d" % nn)
@@ -69,25 +76,25 @@ def main(**kwargs):
             k = len(xx)
             x = xx[:k//nn*nn].reshape(-1, nn).mean(-1)
 
-            print ("\t\t...Step 1")
+            print ("\t\tCompute median/stdev")
             snr = (x.max() - np.median(x))/(np.std(x))
             if snr > SS1[nn]:
                 SS1[nn] = snr
 
-            print ("\t\t...Step 2")
-            med = compute_mom(x)
-            snr = (x.max() - med)/(1.48*compute_mad(x, med))
+            print ("\t\tCompute mom/stdev")
+            snr = (x.max() - compute_mom(x))/(np.std(x))
             if snr > SS2[nn]:
                 SS2[nn] = snr
 
-            print ("\t\t...Step 3")
-            med = np.median(x)
+            print ("\t\tCompute mom/mad")
+            med = compute_mom(x)
             snr = (x.max() - med)/(1.48*compute_mad(x, med))
             if snr > SS3[nn]:
                 SS3[nn] = snr
 
-            print ("\t\t...Step 4")
-            snr = (x.max() - compute_mom(x))/(np.std(x))
+            print ("\t\tCompute median/mad")
+            med = np.median(x)
+            snr = (x.max() - med)/(1.48*compute_mad(x, med))
             if snr > SS4[nn]:
                 SS4[nn] = snr
 
@@ -110,9 +117,12 @@ def make_plot(S1, S2, S3, S4, N, **kwargs):
     plt.plot(25000./N, S3)
     plt.plot(25000./N, S4)
 
-    plt.legend(['median, stdev','MoM, MAD','median, MAD', 'MoM, stdev'], fontsize=20)
-    plt.ylabel('S/N Max (1000 DMs)', fontsize=15)
-    plt.xlabel('N', fontsize=20)
+    plt.legend(['median, stdev','MoM, MAD','median, MAD', 'MoM, stdev'], font_size=kwargs['font_size'])
+    plt.xlabel('N', font_size=kwargs['font_size'])
+    if kwargs['input_type'] is 'time_chunks':
+        plt.ylabel('S/N Max (%d samples, %d chunk size)' % (kwargs['n_samples'], kwargs['chunk_size']), font_size=kwargs['font_size'])
+    else:
+        plt.ylabel('S/N Max (%d samples)' % (kwargs['n_samples']), font_size=kwargs['font_size'])
 
     plt.xscale = kwargs['xscale']
     plt.yscale = kwargs['yscale']
@@ -128,14 +138,21 @@ def check_defaults(**kwargs):
     if kwargs['yscale'] == None:
         kwargs['yscale'] = 'linear'
 
-    if kwargs['N_DM'] == None:
-        kwargs['N_DM'] = 1000
+    if kwargs['font_size'] == None:
+        kwargs['font_size'] = 12
+
+    if kwargs['n_samples'] == None:
+        kwargs['n_samples'] = 1000
+    elif kwargs['input_type'] is 'time_chunks':
+        kwargs['n_samples'] = 200
 
     if kwargs['figure_filename'] == None:
-        kwargs['figure_filename'] = 'filterbank_%d.png' % kwargs['N_DM']
+        kwargs['figure_filename'] = 'filterbank_%d.png' % kwargs['n_samples']
 
-    if kwargs['gaussian_noise'] == None:
+    if kwargs['input_type'] in ('filterbank', 'time_chunks'):
         kwargs['gaussian_noise'] = False
+
+    kwargs['filterbank_filename'] = '/data2/output/20181020_2018-10-20-09:59:59.FRB171004_filterbank/CB17.fil'
 
     return kwargs
 
@@ -145,10 +162,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--pickle_filename', help="Pickle filename")
     parser.add_argument('--figure_filename', help="Figure filename", type=str)
+    parser.add_argument('--input_type',
+                        help="""Options:
+                        {'filterbank': Filterbank file (.fil),
+                        'gaussian_noise': Stochastic gaussian draw,
+                        'time_chunks': Discrete time chunks of size [index_start, index_start+chunk_size]}
+                             """,
+                        type=str, choices=['filterbank', 'gaussian_noise', 'time_chunks'])
+    parser.add_argument('--chunk_size', help="Chunk size (if input_type is 'time_chunks')", type=int)
+    parser.add_argument('--font_size', help="Figure's font size", type=int)
     parser.add_argument('--gaussian_noise', help="Use gaussian noise input (True/False)", type=bool)
-    parser.add_argument('--N_DM', help="Number of DM trials", type=int)
-    parser.add_argument('--xscale', help="Scaling for the x axis", type=str)
-    parser.add_argument('--yscale', help="Scaling for the y axis", type=str)
+    parser.add_argument('--n_samples', help="Number of trials (e.g. DMs, draws, chunks)", type=int)
+    parser.add_argument('--xscale', help="Scaling for the x axis", type=str, choices=['linear', 'log'])
+    parser.add_argument('--yscale', help="Scaling for the y axis", type=str, choices=['linear', 'log'])
     args = parser.parse_args()
 
     kwargs = check_defaults(**vars(args))
